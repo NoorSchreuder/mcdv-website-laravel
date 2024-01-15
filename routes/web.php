d3b33183a7de8f03a6c0e2d702fb8ad33ceb0953
Noor Schreuder <noor2302@gmail.com>
2024-01-15 20:30:49 +0100

    index

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,50 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
+});
+
+Route::get('/appartement', function () {
+    return view('appartement');
+});
+
+Route::get('/badkamer', function () {
+    return view('badkamer');
+});
+
+Route::get('/buiten', function () {
+    return view('buiten');
+});
+
+Route::get('/contact', function () {
+    return view('contact');
+});
+
+Route::get('/fotos', function () {
+    return view('fotos');
+});
+
+Route::get('/meer-informatie', function () {
+    return view('meer-informatie');
+});
+
+Route::get('/over-ons', function () {
+    return view('over-ons');
+});
+
+Route::get('/routebeschrijving', function () {
+    return view('routebeschrijving');
+});
+
+Route::get('/slaapkamer', function () {
+    return view('slaapkamer');
+});
+
+Route::get('/wat-te-doen', function () {
+    return view('wat-te-doen');
+});
+
+Route::get('/woonkamer', function () {
+    return view('woonkamer');
 });
