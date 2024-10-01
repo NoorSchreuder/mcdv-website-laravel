69acba4ea4a48159a25d20de7d09c178eda99636
Robin Pijnappels <73393404+RobinPijnappelsAvans@users.noreply.github.com>
2024-10-01 10:58:45 +0200

    Update asset file paths in layout.blade.php

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -5,8 +5,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="description"
-        content="Ontdek de charme van het Italiaanse plattelandsleven bij Il Bandito Arancione, een prachtig appartement in een 150 jaar oude villa in Umbrië. 
-        Beleef de authentieke Italiaanse cultuur, geniet van onze cateringdiensten, en verken de prachtige omgeving van Monte Castello di Vibio. 
+        content="Ontdek de charme van het Italiaanse plattelandsleven bij Il Bandito Arancione, een prachtig appartement in een 150 jaar oude villa in Umbrië.
+        Beleef de authentieke Italiaanse cultuur, geniet van onze cateringdiensten, en verken de prachtige omgeving van Monte Castello di Vibio.
         Perfect voor een vredige en pittoreske vakantie">
     <meta name="keywords"
         content="Il Bandito Arancione,
@@ -23,25 +23,23 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title> Il Bandito Arancione </title>
 
-
     <link href="https://fonts.googleapis.com/css?family=Lora:400,700&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Cabin:400,500,600,700&display=swap" rel="stylesheet">
 
-
-    <link rel="stylesheet" href="css/bootstrap.min.css" type="text/css">
-    <link rel="stylesheet" href="css/font-awesome.min.css" type="text/css">
-    <link rel="stylesheet" href="css/elegant-icons.css" type="text/css">
-    <link rel="stylesheet" href="css/flaticon.css" type="text/css">
-    <link rel="stylesheet" href="css/owl.carousel.min.css" type="text/css">
-    <link rel="stylesheet" href="css/nice-select.css" type="text/css">
-    <link rel="stylesheet" href="css/jquery-ui.min.css" type="text/css">
-    <link rel="stylesheet" href="css/magnific-popup.css" type="text/css">
-    <link rel="stylesheet" href="css/slicknav.min.css" type="text/css">
-    <link rel="stylesheet" href="css/style.css" type="text/css">
-    <link rel="apple-touch-icon" sizes="180x180" href="img/favicon/apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="img/favicon/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="img/favicon/favicon-16x16.png">
-    <link rel="manifest" href="img/favicon/site.webmanifest">
+    <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}?v=1" type="text/css">
+    <link rel="stylesheet" href="{{ asset('css/font-awesome.min.css') }}?v=1" type="text/css">
+    <link rel="stylesheet" href="{{ asset('css/elegant-icons.css') }}?v=1" type="text/css">
+    <link rel="stylesheet" href="{{ asset('css/flaticon.css') }}?v=1" type="text/css">
+    <link rel="stylesheet" href="{{ asset('css/owl.carousel.min.css') }}?v=1" type="text/css">
+    <link rel="stylesheet" href="{{ asset('css/nice-select.css') }}?v=1" type="text/css">
+    <link rel="stylesheet" href="{{ asset('css/jquery-ui.min.css') }}?v=1" type="text/css">
+    <link rel="stylesheet" href="{{ asset('css/magnific-popup.css') }}?v=1" type="text/css">
+    <link rel="stylesheet" href="{{ asset('css/slicknav.min.css') }}?v=1" type="text/css">
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}?v=1" type="text/css">
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('img/favicon/apple-touch-icon.png') }}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('img/favicon/favicon-32x32.png') }}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('img/favicon/favicon-16x16.png') }}">
+    <link rel="manifest" href="{{ asset('img/favicon/site.webmanifest') }}">
     <meta name="msapplication-TileColor" content="#da532c">
     <meta name="theme-color" content="#ffffff">
 </head>
